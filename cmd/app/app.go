f2906692e937e34e659f52dc2fee0bb496d68100
Cleyson Lima <cleysonph@gmail.com>
2022-12-06 01:40:05 -0300

    feat: implement the rest executable

---
+++ cmd/app/app.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/CleysonPH/reading-tracker/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ internal/app/rest.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/CleysonPH/reading-tracker/config"
+	"github.com/CleysonPH/reading-tracker/internal/database"
+	"github.com/CleysonPH/reading-tracker/internal/repository"
+	"github.com/CleysonPH/reading-tracker/internal/transport/rest"
+)
+
+func Run() {
+	flag.StringVar(&config.Port, "port", config.Port, "Port to run the server on")
+	flag.StringVar(&config.Env, "env", config.Env, "Environment to run the server on")
+	flag.StringVar(&config.Dsn, "dsn", config.Dsn, "Data source name to connect to the database")
+	flag.StringVar(&config.Host, "host", config.Host, "Host to run the server on")
+	flag.Parse()
+
+	database.InitMysql(config.Dsn)
+	db, err := database.GetDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	bookRepository := repository.NewBookModel(db)
+	bookHandler := rest.NewBookHandler(bookRepository)
+
+	router := rest.NewRouter(bookHandler)
+
+	srv := &http.Server{
+		Addr:    config.Addr(),
+		Handler: router,
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
