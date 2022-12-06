f287db49a3248f4db325c5adc32276d0e41dc79e
Cleyson Lima <cleysonph@gmail.com>
2022-12-06 00:15:57 -0300

    feat: implement the funtion to get all books in db

--- internal/models/book.go
+++ internal/models/book.go
@@ -24,3 +24,83 @@ type Book struct {
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 }
+
+type BookModel interface {
+	All(q string) ([]*Book, error)
+}
+
+type bookMySQlModel struct {
+	db *sql.DB
+}
+
+// All implements BookModel
+func (m *bookMySQlModel) All(q string) ([]*Book, error) {
+	stmt := `
+		SELECT
+			id,
+			title,
+			subtitle,
+			isbn,
+			authors,
+			categories,
+			language,
+			cover,
+			publisher,
+			published_at,
+			pages,
+			read_pages,
+			description,
+			reading_status,
+			edition,
+			created_at,
+			updated_at
+		FROM
+			books
+		WHERE
+			LOWER(title) LIKE CONCAT('%', LOWER(?), '%')
+	`
+
+	rows, err := m.db.Query(stmt, q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	books := []*Book{}
+	for rows.Next() {
+		b := &Book{}
+		err := rows.Scan(
+			&b.ID,
+			&b.Title,
+			&b.Subtitle,
+			&b.Isbn,
+			&b.Authors,
+			&b.Categories,
+			&b.Language,
+			&b.Cover,
+			&b.Publisher,
+			&b.PublishedAt,
+			&b.Pages,
+			&b.ReadPages,
+			&b.Description,
+			&b.ReadingStatus,
+			&b.Edition,
+			&b.CreatedAt,
+			&b.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		books = append(books, b)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return books, nil
+}
+
+func NewBookModel(db *sql.DB) BookModel {
+	return &bookMySQlModel{db}
+}
