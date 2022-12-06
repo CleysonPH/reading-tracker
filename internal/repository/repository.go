e242431f7241fb8bfbc43f4557e8c46b21b2b5fd
Cleyson Lima <cleysonph@gmail.com>
2022-12-06 03:15:00 -0300

    feat: implement delete book repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,4 +5,5 @@ import "github.com/CleysonPH/reading-tracker/internal/model"
 type BookRepository interface {
 	All(q string) ([]*model.Book, error)
 	Get(id int64) (*model.Book, error)
+	Delete(id int64) error
 }
